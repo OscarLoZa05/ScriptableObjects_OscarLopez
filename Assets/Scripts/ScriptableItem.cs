b1e7d792b0f701e2d394cf9a3752efaf5cddb7f2
OscarLoZa05 <oscarlzafra@gmail.com>
2026-04-01 18:29:06 +0200

    Items

---
+++ Assets/Scripts/ScriptableItem.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Item", menuName = "ScriptableObjects/Items")]
+public class ScriptableItem : ScriptableObject
+{
+
+    public string spanishTranslation;
+    public string englishTranslation;
+    public string catalanTranslation;  
+}
