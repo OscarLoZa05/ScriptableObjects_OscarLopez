03af20cd3ee8faf7a32d9194cadeaff389938ba1
OscarLoZa05 <oscarlzafra@gmail.com>
2026-04-07 02:11:08 +0200

    Final

--- Assets/Scripts/TranslationManager.cs
+++ Assets/Scripts/TranslationManager.cs
@@ -20,19 +20,6 @@ public class TranslationManager : MonoBehaviour
     public ScriptableItem[] items;
     public Text[] itemsNames;
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void Translation(int indexTranslation)
     {
         for (int i = 0; i < items.Length; i++)
@@ -40,6 +27,4 @@ public class TranslationManager : MonoBehaviour
             itemsNames[i].text = items[i].Translations[indexTranslation].ToString();
         }
     }
-
-
 }
